aebf504e8a17e5d1e987ca7024711bf91ff9903e
Edwin Meijne <git@edwinmeijne.nl>
2016-05-15 09:57:07 +0200

    * after testing, added a better way to print to the println (as one print, not many)

--- Arduino/Ed-2000_simpleSerial/Ed-2000_simpleSerial.ino
+++ Arduino/Ed-2000_simpleSerial/Ed-2000_simpleSerial.ino
@@ -16,23 +16,26 @@ int Right_X_Val = 0;
 int Right_Y_Val = 0;
 int Right_Z_Val = 0;
 
+String divider = ",";
+
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  // Read and remap the value to midi-like range 0-127 and print it to the serial, ending with a println
-  // The Serial.println gives the analog converter some time to recover as well
-  // normally you would have to call a delay(1) between reads
-  Serial.print(map(analogRead(Left_X), 0, 1023, 0, 127));
-  Serial.print(',');
-  Serial.print(map(analogRead(Left_Y), 0, 1023, 0, 127));
-  Serial.print(',');
-  Serial.print(map(analogRead(Left_Z), 0, 1023, 0, 127));
-  Serial.print(',');
-  Serial.print(map(analogRead(Right_X), 0, 1023, 0, 127));
-  Serial.print(',');
-  Serial.print(map(analogRead(Right_Y), 0, 1023, 0, 127));
-  Serial.print(',');
-  Serial.println(map(analogRead(Right_Z), 0, 1023, 0, 127));
+  // Read the values of each pin with a delay to give the analog converter time to recover in between
+  Left_X_Val = analogRead(Left_X);
+  delay(1);
+  Left_Y_Val = analogRead(Left_Y);
+  delay(1);
+  Left_Z_Val = analogRead(Left_Z);
+  delay(1);
+  Right_X_Val = analogRead(Right_X);
+  delay(1);
+  Right_Y_Val = analogRead(Right_Y);
+  delay(1);
+  Right_Z_Val = analogRead(Right_Z);
+  delay(1);
+  // Print to a line seperated by divider
+  Serial.println(Left_X_Val + divider + Left_Y_Val + divider + Left_Z_Val + divider + Right_X_Val + divider + Right_Y_Val + divider + Right_Z_Val);
 }
