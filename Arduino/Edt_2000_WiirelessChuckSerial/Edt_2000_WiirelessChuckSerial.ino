b5fcdfcffc1880ea9f767f1a746303b8a72838e4
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-06-11 11:17:25 +0200

    Changes to file names and new Wireless Chuck project

---
+++ Arduino/Edt_2000_WiirelessChuckSerial/Edt_2000_WiirelessChuckSerial.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include "nunchuck_funcs.h"
+
+int loop_cnt = 0;
+
+byte accx, accy, zbut, cbut, joyx, joyy;
+#define LED 13;
+
+String divider = " ";
+
+
+void setup()
+{
+	Serial.begin(57600);
+	nunchuck_setpowerpins();
+	nunchuck_init(); // send the initilization handshake
+}
+
+void loop()
+{
+	nunchuck_get_data();
+
+	joyx = nunchuck_joyx();
+	joyy = nunchuck_joyy();
+	zbut = nunchuck_zbutton();
+	cbut = nunchuck_cbutton();
+	Serial.println(
+		int((byte)joyx) + divider +
+		int((byte)joyy) + divider +
+		int((byte)zbut) + divider +
+		int((byte)cbut)
+		);
+
+	delay(1);
+}
