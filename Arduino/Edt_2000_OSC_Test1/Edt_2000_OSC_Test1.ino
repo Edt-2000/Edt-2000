3aaab432ef5cd11353f981c836c25db8f47ce5a6
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-06-15 21:57:40 +0200

    New test project

---
+++ Arduino/Edt_2000_OSC_Test1/Edt_2000_OSC_Test1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Edt_2000_OSC_Test1.ino
+ Created:	6/15/2016 9:36:13 PM
+ Author:	thoma
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
