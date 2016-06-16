e3632f9eb21e50e31c8b7689bb7180d640b7b10f
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-06-16 15:05:14 +0200

    OSC mirror and updated OSC server.
    Button gets clicked -> red LED on -> server sends message to mirror -> server reads message from mirror -> green LED on.
    Button gets released -> red LED off -> server sends message to mirror -> server reads message from mirror -> green LED off.
    
    If mesage exchange is slow, the rgb LED is either red or green during exchange and yellow after exchange is finished.
    If message exchange is fast, the rgb LED is only yellow.

--- Arduino/Edt_2000_OSC_Test2Server/Edt_2000_OSC_Test2Server.ino
+++ Arduino/Edt_2000_OSC_Test2Server/Edt_2000_OSC_Test2Server.ino
@@ -5,15 +5,24 @@
 
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
+IPAddress mirror = { 192, 168, 0, 100 };
+
 int serverPort = 8000;
 int destPort = 9000;
+
+int redLEDPin = 5;
+int greenLEDPin = 6;
+
+int localButtonPin = 8;
+bool localButton = false;
 
 EthernetUDP Udp;
 
 void setup() {
-	pinMode(5, OUTPUT);
-	pinMode(6, OUTPUT);
-	pinMode(9, OUTPUT);
+	pinMode(localButtonPin, INPUT_PULLUP);
+
+	pinMode(redLEDPin, OUTPUT);
+	pinMode(greenLEDPin, OUTPUT);
 
 	Serial.begin(9600);
 	Serial.println("OSC test");
@@ -35,6 +44,21 @@ void setup() {
 
 void loop() {
 	OSCMsgReceive();
+
+	if (localButton != digitalRead(localButtonPin)) {
+		localButton = !localButton;
+
+		digitalWrite(redLEDPin, !localButton);
+
+		OSCMessage msgOUT("/Button/1");
+
+		msgOUT.add<int>(!localButton);
+
+		Udp.beginPacket(mirror, destPort);
+		msgOUT.send(Udp);
+		Udp.endPacket();
+		msgOUT.empty();
+	}
 }
 
 void OSCMsgReceive() {
@@ -50,31 +74,13 @@ void OSCMsgReceive() {
 		Serial.println();
 
 		if (!msgIN.hasError()) {
-			// metroOSC cannot be configured...
-			msgIN.route("/Button1", toggleOnOff);
-			msgIN.route("/Button2", toggleOnOff);
-			msgIN.route("/Button3", toggleOnOff);
+			msgIN.route("/Button/1", toggleOnOff);
 		}
 	}
 }
 
 void toggleOnOff(OSCMessage &msg, int addrOffset) {
 	int state = msg.getInt(0);
-
-	// stripping /Button from address
-	char buffer[1];
-	msg.getAddress(buffer, 7);
-
-	if (buffer[0] == '1') {
-		digitalWrite(5, state);
-	}
-
-	if (buffer[0] == '2') {
-		digitalWrite(6, state);
-	}
-
-	if (buffer[0] == '3') {
-		digitalWrite(9, state);
-	}
+	digitalWrite(greenLEDPin, state);
 }
 
---
+++ CSharp/Edt_2000_OCS_Mirror/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+using SharpOSC;
+
+namespace Edt_2000_OCS_Mirror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var sender = new UDPSender("192.168.0.102", 8000);
+                
+                // The cabllback function
+                HandleOscPacket callback = delegate (OscPacket packet)
+                {
+                    var messageReceived = (OscMessage)packet;
+                    sender.Send(messageReceived);
+                };
+
+                var listener = new UDPListener(9000, callback);
+
+                Console.WriteLine("Press enter to stop");
+                Console.ReadLine();
+                listener.Close();
+            }
+        }
+    }
+}
