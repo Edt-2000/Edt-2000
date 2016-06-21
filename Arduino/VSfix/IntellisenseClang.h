cc5303b99b7553796571e7dac3050afea8808220
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-06-21 16:25:43 +0200

    Fix for visual studio.

---
+++ Arduino/VSfix/IntellisenseClang.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifdef _MSC_VER
+#define __asm__(x)
+#define __extension__(x)
+#define __attribute__(x)
+#define __builtin_va_list int
+#define __extension__
+#define __inline__
+#define __builtin_constant_p
+#define _Bool bool
+#undef __cplusplus
+#endif
