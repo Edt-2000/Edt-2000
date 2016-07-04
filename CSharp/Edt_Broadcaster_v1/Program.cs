e22d627018ea0be0b54702eb4298026e7780bad8
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-07-04 23:46:56 +0200

    Changes to monitoring application

--- CSharp/Edt_Broadcaster_v1/Program.cs
+++ CSharp/Edt_Broadcaster_v1/Program.cs
@@ -4,6 +4,7 @@
  * This broadcaster is used for monitoring and relaying. Multicast should be enabled on all nodes sending UDP messages, making the relaying functionality superfluous.
  */
 using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,29 +17,45 @@ namespace Edt_Broadcaster_v1
 	class Program
 	{
 		static DateTime previous;
+		static int messages;
 		
-		static string[] broadcastList = { "192.168.0.120", "192.168.0.121" };
-		static List<UDPSender> broadcastSenders = new List<UDPSender>();
+		//static string[] broadcastList = { "192.168.0.120", "192.168.0.121" };
+		//static List<UDPSender> broadcastSenders = new List<UDPSender>();
 
 		static void Main(string[] args)
 		{
-			foreach(string ip in broadcastList)
+			/*foreach(string ip in broadcastList)
 			{
 				broadcastSenders.Add(new UDPSender(ip, 8000));
-			}
+			}*/
 
+            
 			while (true)
 			{
 				previous = DateTime.Now;
+				messages = 0;
 
 				// The cabllback function
 				HandleOscPacket callback = delegate (OscPacket packet)
 				{
 					var messageReceived = (OscMessage)packet;
 
-					Console.WriteLine(messageReceived.Address + " - " + messageReceived.OriginEP.Address + " - "  + messageReceived.OriginEP.Port + " - "  + string.Join(",",messageReceived.Arguments));
+					messages++;
 
-					foreach(UDPSender sender in broadcastSenders)
+					if(DateTime.Now.Subtract(previous).TotalMilliseconds > 1000)
+					{
+						previous = DateTime.Now;
+
+						Console.WriteLine(messages + " per second. " + (1000.0 / Math.Max(1,messages)) + " ms per message.");
+
+						Console.WriteLine(messageReceived.Address + " - " + messageReceived.OriginEP.Address + " - " + messageReceived.OriginEP.Port + " - " + string.Join(",", messageReceived.Arguments));
+
+						messages = 0;
+					}
+
+					//Console.WriteLine(messageReceived.Address + " - " + messageReceived.OriginEP.Address + " - "  + messageReceived.OriginEP.Port + " - "  + string.Join(",",messageReceived.Arguments));
+
+					/*foreach(UDPSender sender in broadcastSenders)
 					{
 						if(sender.Address != messageReceived.OriginEP.Address.ToString())
 						{
@@ -46,10 +63,11 @@ namespace Edt_Broadcaster_v1
 
 							sender.Send(messageReceived);
 						}
-					}
+					}*/
 				};
 
-				var listener = new UDPListener(9000, callback);
+				var listener = new UDPListener(12345, IPAddress.Parse("239.255.255.250"), callback);
+                
 				
 				Console.WriteLine("Press enter to stop");
 				Console.ReadLine();
