3ce970204ab1e586a9a2b544bd0d2f489a3e8e82
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-07-05 23:21:10 +0200

    Added global definition file

---
+++ Arduino/libraries/Edt-lib/Definitions.h
@@ -0,0 +1,11 @@
+/* Definition file */
+
+#define IP_TRAK IPAddress(192, 168, 0, 120)
+#define IP_SUIT_1 IPAddress(192, 168, 0, 121)
+#define IP_INTERFACE IPAddress(0, 0, 0, 0)
+#define IP_MULTICAST IPAddress(239, 255, 255, 250)
+
+#define PORT_MULTICAST uint16_t(12345)
+
+// instrument / input OSC names
+#define OSC_TRAK "/Trak"
