cded398191b8e4345bebf46123e376e0fa181088
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-07-14 18:14:44 +0200

    Fixed the chuk library. Accelerometer data is omited as it requires a calibration during init, but we do not use the accelerometer, so it would be waste of time. EdtOSCChuk should work the same as EdtI2CChuk, the only difference is its source of data.

--- Arduino/libraries/Edt-lib/Chuk.h
+++ Arduino/libraries/Edt-lib/Chuk.h
@@ -15,37 +15,69 @@ http://www.windmeadow.com/node/42
 #include "OSC.h"
 #include "Wire.h"
 
-union EdtOSCChukData
+union EdtOSCChuckData
+{
+public:
+	float buffer[4];
+
+	EdtOSCChuckData() {
+		reset();
+	}
+
+	inline float joyX() { return _joyX; };
+	inline float joyY() { return _joyY; };
+	inline float buttonC() { return _buttonC; };
+	inline float buttonZ() { return _buttonZ; };
+
+	void reset() {
+		_buttonC = 0.0;
+		_buttonZ = 0.0;
+		_joyX = 0.0;
+		_joyY = 0.0;
+	}
+
+private:
+	float _buttonC;
+	float _buttonZ;
+	float _joyX;
+	float _joyY;
+};
+
+union EdtI2CChukData
 {
 public:
 	byte buffer[6];
 
-	EdtOSCChukData() {
+	EdtI2CChukData() {
 		reset();
 	};
 
 	// normalization to -1.0 to 1.0 float (TouchOSC only supports floats)
-	float joyX() { return max(-1.0, min(1.0, ((_joyX / 128.0) - 1.0) / 0.70)); };
-	float joyY() { return max(-1.0, min(1.0, ((_joyY / 128.0) - 1.0) / 0.70)); };
+	float joyX() { return (float)_joyX; }; // return max(-1.0, min(1.0, ((_joyX / 128.0) - 1.0) / 0.70)); };
+	float joyY() { return (float)_joyY; }; // max(-1.0, min(1.0, ((_joyY / 128.0) - 1.0) / 0.70)); };
 	float buttonC() { return (float)!_buttonC; };
 	float buttonZ() { return (float)!_buttonZ; };
 
 	// this is not normalized and not calibrated
-	float accellX() { return (((float)_accellX * 4.0 / 128.0) + ((float)_lsbAccellX / 128.0)); };
+	/*float accellX() { return (((float)_accellX * 4.0 / 128.0) + ((float)_lsbAccellX / 128.0)); };
 	float accellY() { return (((float)_accellY * 4.0 / 128.0) + ((float)_lsbAccellY / 128.0)); };
-	float accellZ() { return (((float)_accellZ * 4.0 / 128.0) + ((float)_lsbAccellZ / 128.0)); };
+	float accellZ() { return (((float)_accellZ * 4.0 / 128.0) + ((float)_lsbAccellZ / 128.0)); };*/
 
 	void reset() {
-		_joyX = 0;
-		_joyY = 0;
+		// reset to center
+		_joyX = 128.0;
+		_joyY = 128.0;
+
 		_accellX = 0;
 		_accellY = 0;
 		_accellZ = 0;
+
 		_buttonC = false;
 		_buttonZ = false;
-		_lsbAccellX = 0;
-		_lsbAccellY = 0;
-		_lsbAccellZ = 0;
+
+		//_lsbAccellX = 0;
+		//_lsbAccellY = 0;
+		//_lsbAccellZ = 0;
 	};
 
 private:
@@ -53,7 +85,7 @@ private:
 	struct {
 		unsigned int _joyX : 8;
 		unsigned int _joyY : 8;
-		
+
 		unsigned int _accellX : 8;
 		unsigned int _accellY : 8;
 		unsigned int _accellZ : 8;
@@ -62,31 +94,48 @@ private:
 		bool _buttonC : 1;
 
 		// LSB bits of accellerometer data
-		unsigned int _lsbAccellX : 2; 
-		unsigned int _lsbAccellY : 2;
-		unsigned int _lsbAccellZ : 2;
+		//unsigned int _lsbAccellX : 2;
+		//unsigned int _lsbAccellY : 2;
+		//unsigned int _lsbAccellZ : 2;
 	};
 };
 
 class EdtOSCChuk : public EdtOSCRoutingObject
 {
 public:
-	EdtOSCChukData data = EdtOSCChukData();
+	EdtOSCChuckData data = EdtOSCChuckData();
+
+	EdtOSCChuk(char * pattern) {
+		_pattern = pattern;
+	}
+
+	const char * OSCPattern() {
+		return _pattern;
+	}
+
+	void OSCCallback(OSCMessage &msg, int addrOffset) {
+		for (int i = 0; i < 4; i++) {
+			data.buffer[i] = msg.getFloat(i);
+		}
+	}
+private:
+	const char * _pattern;
 };
 
-class EdtChuk
+class EdtI2CChuk : public EdtOSCSourceObject
 {
 public:
-	EdtOSCChukData data = EdtOSCChukData();
+	EdtI2CChukData data = EdtI2CChukData();
 
-	EdtChuk(int address) {
-		_address = address;
+	EdtI2CChuk(int i2cAddress, const char * oscAddress) {
+		_i2cAddress = i2cAddress;
+		_oscAddress = oscAddress;
 		_requested = false;
 	}
 
 	void begin() {
 		Wire.begin();
-		Wire.beginTransmission(_address);
+		Wire.beginTransmission(_i2cAddress);
 		Wire.write((uint8_t)0x40);
 		Wire.write((uint8_t)0x00);
 		Wire.endTransmission();
@@ -98,7 +147,7 @@ public:
 		int bytesReceived = 0;
 
 		// Read 6 bytes from the nunchuck and check 6 bytes were received
-		Wire.requestFrom(_address, 6);
+		Wire.requestFrom(_i2cAddress, 6);
 		while (Wire.available()) {
 			data.buffer[bytesReceived++] = _decodeByte(Wire.read());
 		}
@@ -110,12 +159,27 @@ public:
 		requestData();
 	}
 
+	OSCMessage * OSCSendMessage() {
+		message = new OSCMessage(_oscAddress);
+
+		message
+			->add<float>(data.buttonC())
+			.add<float>(data.buttonZ())
+			.add<float>(data.joyX())
+			.add<float>(data.joyX());
+
+		return message;
+	}
+
+	OSCMessage * message;
+
 private:
-	int _address;
+	int _i2cAddress;
+	const char * _oscAddress;
 	bool _requested;
 
 	void requestData() {
-		Wire.beginTransmission(_address);
+		Wire.beginTransmission(_i2cAddress);
 		Wire.write((uint8_t)0x00);
 		Wire.endTransmission();
 	}
