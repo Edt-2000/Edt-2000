2f5b868bd7fbf4b13d8968abb1e2ccc3588d90e7
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-07-27 21:30:32 +0200

    Playing with the prescaler of the ADC.

--- Arduino/Edt_Trak_v1/src/main.cpp
+++ Arduino/Edt_Trak_v1/src/main.cpp
@@ -22,7 +22,18 @@ EthernetUDP Udp;
 
 EdtAITrak Trak = EdtAITrak(0, 0, 0, 0, 0, 0, OSC_TRAK);
 
+// Define various ADC prescaler
+// http://www.microsmart.co.za/technical/2014/03/01/advanced-arduino-adc/
+const unsigned char PS_16 = (1 << ADPS2);
+const unsigned char PS_32 = (1 << ADPS2) | (1 << ADPS0);
+const unsigned char PS_64 = (1 << ADPS2) | (1 << ADPS1);
+const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
 void setup() {
+	// set up the ADC
+	ADCSRA &= ~PS_128;
+	ADCSRA |= PS_16;
+
 	Statemachine.begin(13, LOW);
 }
 
