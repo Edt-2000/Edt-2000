a52daec9aab59a3fe5e6a2fb4c7d85999e4c2363
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-10-10 21:24:01 +0200

    Some updates to test app

--- CPlusPlus/OSC-light.Test/OSC-light.Test.cpp
+++ CPlusPlus/OSC-light.Test/OSC-light.Test.cpp
@@ -3,46 +3,102 @@
 
 #include "stdafx.h"
 
+#include "Print.h"
+
+#include "..\..\Arduino\libraries\OSC-light\OSCMessage.cpp"
+#include "..\..\Arduino\libraries\OSC-light\OSCMessage.h"
+#include "..\..\Arduino\libraries\OSC-light\OSCMatch.h"
+
+
 int main()
 {
-	OSCMatch tester = OSCMatch();
+	Print print = Print();
 
-	bool tests[24];
-	int i = 0;
-	
-	tests[i++] = tester.isMatch("/Unit1", "/Unit1");
-	tests[i++] = tester.isMatch("/Unit1/Preset", "/Unit1/Preset");
-	tests[i++] = !tester.isMatch("/Unit1", "/Unit2");
-	tests[i++] = !tester.isMatch("/Unit1/Preset", "/Unit2");
-	tests[i++] = tester.isMatch("/Unit1/Preset/a", "/Unit1/Preset/a");
-	tests[i++] = tester.isMatch("/Unit12345", "/Unit_2345");
-	tests[i++] = tester.isMatch("/Unit12345", "/Unit_2_45");
-	tests[i++] = tester.isMatch("/Unit12345", "/Unit_2_4_");
-	tests[i++] = !tester.isMatch("/Unit12345", "/Unit_2_3_");
-	tests[i++] = !tester.isMatch("/Unit12345", "/Unit_2_1_");
-	tests[i++] = tester.isMatch("/Unit12345", "/Unit_2___");
-	tests[i++] = tester.isMatch("/MegaUnitMegaUnitMegaUnitMegaUnit", "/MegaUnit_egaUnitMegaUnitMeg_Unit");
-	tests[i++] = tester.isMatch("/Unit1/Preset", "/Unit_/Preset");
-	tests[i++] = tester.isMatch("/Unit1/Preset", "/_n_t_/Preset");
-	tests[i++] = tester.isMatch("/Unit1/Preset", "/Unit_/Preset");
-	tests[i++] = tester.isMatch("/Unit2/Preset/a", "/Unit_/Preset/a");
-	tests[i++] = tester.isMatch("/Unit1", "/*");
-	tests[i++] = tester.isMatch("/Unit1/Preset", "/Unit1/*");
-	tests[i++] = tester.isMatch("/Unit1/Preset/a", "/Unit1/*/a");
-	tests[i++] = tester.isMatch("/Unit1/Preset/a", "/Unit1/Preset/*");
-	tests[i++] = tester.isMatch("/Unit1/Preset/a", "/*/Preset/a");
-	tests[i++] = !tester.isMatch("/Unit1/Preset", "/Unit1/*/a");
-	tests[i++] = tester.isMatch("/Unit1/Preset/a/b/c", "/Unit1/*/a/*/c");
-	tests[i++] = !tester.isMatch("/Unit1/Preset/a/b/d", "/Unit1/*/a/*/c");
-	tests[i++] = tester.isMatch("/Unit1/Preset/a/c/a", "/*/*/a/*/*");
-	
-	i = 0;
-	for each(bool test in tests) {
-		if (test) {
-			printf("%i test succeeded.\r\n", i++);
+	OSCMatch tester = OSCMatch();
+	OSCMessage message = OSCMessage();
+	OSCMessage newMessage = OSCMessage();
+
+	int repeats = 0;
+
+	while (repeats++ < 1) {
+
+		message.setAddress("/Some/Message/Address123");
+
+		bool tests[50];
+		for (int i = 0; i < 50; ++i) {
+			tests[i] = false;
 		}
-		else {
-			printf("%i test failed.\r\n", i++);
+
+		int i = 0, j = 0;
+
+		tests[i++] = tester.isMatch("/Unit1", "/Unit1");
+		tests[i++] = tester.isMatch("/Unit1/Preset", "/Unit1/Preset");
+		tests[i++] = !tester.isMatch("/Unit1", "/Unit2");
+		tests[i++] = !tester.isMatch("/Unit1/Preset", "/Unit2");
+		tests[i++] = tester.isMatch("/Unit1/Preset/a", "/Unit1/Preset/a");
+		tests[i++] = tester.isMatch("/Unit12345", "/Unit_2345");
+		tests[i++] = tester.isMatch("/Unit12345", "/Unit_2_45");
+		tests[i++] = tester.isMatch("/Unit12345", "/Unit_2_4_");
+		tests[i++] = !tester.isMatch("/Unit12345", "/Unit_2_3_");
+		tests[i++] = !tester.isMatch("/Unit12345", "/Unit_2_1_");
+		tests[i++] = tester.isMatch("/Unit12345", "/Unit_2___");
+		tests[i++] = tester.isMatch("/MegaUnitMegaUnitMegaUnitMegaUnit", "/MegaUnit_egaUnitMegaUnitMeg_Unit");
+		tests[i++] = tester.isMatch("/Unit1/Preset", "/Unit_/Preset");
+		tests[i++] = tester.isMatch("/Unit1/Preset", "/_n_t_/Preset");
+		tests[i++] = tester.isMatch("/Unit1/Preset", "/Unit_/Preset");
+		tests[i++] = tester.isMatch("/Unit2/Preset/a", "/Unit_/Preset/a");
+		tests[i++] = tester.isMatch("/Unit1", "/*");
+		tests[i++] = tester.isMatch("/Unit1/Preset", "/Unit1/*");
+		tests[i++] = tester.isMatch("/Unit1/Preset/a", "/Unit1/*/a");
+		tests[i++] = tester.isMatch("/Unit1/Preset/a", "/Unit1/Preset/*");
+		tests[i++] = tester.isMatch("/Unit1/Preset/a", "/*/Preset/a");
+		tests[i++] = !tester.isMatch("/Unit1/Preset", "/Unit1/*/a");
+		tests[i++] = tester.isMatch("/Unit1/Preset/a/b/c", "/Unit1/*/a/*/c");
+		tests[i++] = !tester.isMatch("/Unit1/Preset/a/b/d", "/Unit1/*/a/*/c");
+		tests[i++] = tester.isMatch("/Unit1/Preset/a/c/a", "/*/*/a/*/*");
+
+		tests[i++] = message.route("/Some/Message/Address123");
+		tests[i++] = message.route("/Some/*/Address123");
+		tests[i++] = message.route("/Some/Message/*");
+		tests[i++] = message.route("/Some/Message/Address___");
+
+		message.reserve(16);
+		message.add<int>(127);
+		message.add<float>(2.3);
+		message.add<float>(3.4);
+		message.add<float>(4.5);
+		message.add<float>(5.6);
+		message.add<int>(4301);
+		message.add<int>(-1);
+		message.add<float>(123.123);
+		message.add<float>(1234.1234);
+		message.add<float>(12345.12345);
+		message.add<float>(123456.123456);
+		message.add<int>(-2);
+		message.add<int>(32767);
+		message.add<float>(-2.1);
+		message.add<float>(-2.0001);
+		message.add<float>(-10.0001);
+
+		message.send(&print);
+		OSCMessage::fill(&newMessage, print.buffer, print.bufferSize);
+		
+		int m = 0;
+
+		for (m = 0; m < 16; m++) {
+			float md = message.getFloat(m);
+			float nmd = newMessage.getFloat(m);
+
+			tests[i++] = md == nmd;
+		}
+		
+		for(j = 0; j < i; j++) {
+			if (tests[j]) {
+				printf("%i test succeeded.\r\n", j);
+			}
+			else {
+				printf("%i test failed.\r\n", i);
+			}
 		}
 	}
 
--- CPlusPlus/OSC-light.Test/stdafx.h
+++ CPlusPlus/OSC-light.Test/stdafx.h
@@ -1,8 +1,3 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
 #include "targetver.h"
@@ -10,8 +5,3 @@
 #include <stdio.h>
 #include <tchar.h>
 #include <string.h>
-
-#include "..\..\Arduino\libraries\OSC-light\OSCMatch.h"
-
-
-// TODO: reference additional headers your program requires here
