d4ddd875f6ccadce9b157f22501a83a746ac3395
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-10-13 12:56:20 +0200

    Updated old names and moved send logic to loop

--- Arduino/libraries/Edt-lib/OSC.h
+++ Arduino/libraries/Edt-lib/OSC.h
@@ -22,9 +22,9 @@ class EdtOSC
 {
 public:
 	EdtOSC() {}
-	EdtOSC(int sources, int objects) {
-		_oscProducers = new IOSCMessageProducer*[objects];
-		_oscConsumers = new IOSCMessageConsumer*[sources];
+	EdtOSC(int consumers, int producers) {
+		_oscConsumers = new IOSCMessageConsumer*[consumers];
+		_oscProducers = new IOSCMessageProducer*[producers];
 	}
 
 	void bindUDP(UDP * udp, IPAddress remoteIP, int remotePort) {
@@ -33,42 +33,48 @@ public:
 		_remotePort = remotePort;
 	}
 
-	void addProducer(IOSCMessageProducer * source) {
-		_oscProducers[_producers++] = source;
+	void addConsumer(IOSCMessageConsumer * consumer) {
+		_oscConsumers[_consumers++] = consumer;
 	}
 
-	void addConsumer(IOSCMessageConsumer * object) {
-		_oscConsumers[_consumers++] = object;
+	void addProducer(IOSCMessageProducer * producer) {
+		_oscProducers[_producers++] = producer;
 	}
 
 	void loop() {
 		int i;
 		int size;
 
-		for (i = 0; i < _producers; ++i) {
-			send(_oscProducers[i]->generateMessage());
+		// first get all the messages out
+		while(i < _producers) {
+			OSCMessage * message = _oscProducers[i]->generateMessage();
+
+			if (message != nullptr) {
+				_udpHandle->beginPacket(_remoteIP, _remotePort);
+				message->send(_udpHandle);
+				_udpHandle->endPacket();
+				message->empty();
+			}
+
+			++i;
 		}
 
+		// then process all the messages in
 		if (_consumers > 0) {
 			if ((size = _udpHandle->parsePacket()) > 0) {
-				// size exceeds current buffer. resize buffer to fit incoming message
-				if (size > _udpBufferSize) {
-					delete[] _udpBuffer;
 
-					_udpBufferSize = size;
-					_udpBuffer = new char[_udpBufferSize];
-				}
+				// make sure buffer is big enough
+				_messageIN.reserveForProcess(size);
 
 				// write udp data to buffer
-				_udpHandle->read(_udpBuffer, size);
+				_udpHandle->read(_messageIN.processBuffer, size);
 
-				// feed udp data via buffer
 				// reuse the same message everytime to save repetitive memory allocations
-				_messageIN.fill(_udpBuffer, size);
+				_messageIN.process();
 
 				i = 0;
 				do {
-					if (_messageIN.route(_oscConsumers[i]->address())) {
+					if (_messageIN.isValidRoute(_oscConsumers[i]->address())) {
 						_oscConsumers[i]->callback(&_messageIN);
 					}
 				} while (++i < _consumers);
@@ -80,18 +86,8 @@ public:
 			_udpHandle->flush();
 		}
 	}
-
-	void send(OSCMessage * message) {
-		_udpHandle->beginPacket(_remoteIP, _remotePort);
-		message->send(_udpHandle);
-		_udpHandle->endPacket();
-		message->empty();
-	}
 private:
 	UDP * _udpHandle;
-
-	char * _udpBuffer = new char[16];
-	int _udpBufferSize = 16;
 
 	OSCMessage _messageIN = OSCMessage();
 
