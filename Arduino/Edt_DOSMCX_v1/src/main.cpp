e6ab7e11ee36888e2ce47865d3019dad4bc794c5
Thomas Bleijendaal <thomasbleijendaal@outlook.com>
2016-12-20 20:43:55 +0100

    Empty project

--- Arduino/Edt_DOSMCX_v1/src/main.cpp
+++ Arduino/Edt_DOSMCX_v1/src/main.cpp
@@ -1,10 +1,9 @@
 /*
-Edt-Tree
+Edt-DOSMCX
 
 Using PlatformIO
 */
 #define VERSION "v1"
-#define DEBUG
 
 #include "Definitions.h"
 
@@ -15,115 +14,35 @@ Using PlatformIO
 #include "Statemachine.h"
 #include "Time.h"
 
-#include "Chuk.h"
+#include "Trak.h"
+#include "Pedal.h"
 
 EthernetUDP Udp;
 OSC::Arduino Osc;
 
-EdtOSCChuk Chuk = EdtOSCChuk(OSC_SUIT_CHUK);
-
 void setup() {
-	Statemachine.begin(5, HIGH);
+	Statemachine.setup(13, LOW);
 }
 
 void loop() {
-	Statemachine.loop();
-
 	if (Statemachine.isBegin()) {
 		Time.begin();
+		Statemachine.begin();
 
-#ifdef DEBUG
-		Serial.begin(345600);
+		Ethernet.begin(MAC_TRAK);
 
-		// Trak code
-		Serial.print("Edt-DOSMCX");
-		Serial.println(VERSION);
-
-		Serial.println("Starting Ethernet..");
-#endif
-		Ethernet.begin(MAC_DOSMCX);
-#ifdef DEBUG
-		Serial.println("Started Ethernet.");
-
-		Serial.print("IP: ");
-		for (byte thisByte = 0; thisByte < 4; thisByte++) {
-			Serial.print(Ethernet.localIP()[thisByte], DEC);
-			Serial.print(".");
-		}
-		Serial.println();
-
-		Serial.println("Starting UDP..");
-#endif
 		Udp.begin(PORT_BROADCAST);
-#ifdef DEBUG
-		Serial.println("Started UDP.");
 
-		Serial.println("Starting code..");
-#endif
-
-		Osc = OSC::Arduino(1, 0);
+		Osc = OSC::Arduino(0, 2);
 		Osc.bindUDP(&Udp, IP_BROADCAST, PORT_BROADCAST);
-		Osc.addConsumer(&Chuk);
-
-#ifdef DEBUG
-		Serial.println("Started code.");
-#endif
-		// /Trak code
 
 		Statemachine.ready();
 	}
 	else {
-
-		int output = 0;
-		bool strobo = false;
-		unsigned int stroboCycle = 0;
-		unsigned int stroboLength = 8096;
-
 		while (Statemachine.isRun()) {
 			Time.loop();
 
 			Osc.loop(Time.tOSC);
-
-			if (!Chuk.data.buttonC() && !Chuk.data.buttonZ()) {
-				output = 255;
-
-				strobo = Chuk.data.joyY() > 0;
-
-				if (strobo) {
-					stroboLength = 8096 - (Chuk.data.joyY() * 48);
-				}
-				else {
-					stroboLength = 8096;
-				}
-
-				if (Chuk.data.joyY() < 0) {
-					output += max(-255, Chuk.data.joyY() * 3);
-				}
-
-				if (Chuk.data.joyX() > 40) {
-					output = max(output, pow(((double)stroboCycle) / ((double)stroboLength), 2.0) * 255);
-				}
-			}
-			else {
-				strobo = false;
-				output = 0;
-			}
-
-			stroboCycle++;
-
-			if (strobo) {
-				//PWM.set((stroboCycle < stroboLength) ? output : 0);
-			}
-			else {
-				//PWM.set(output);
-			}
-
-			if (stroboCycle > 2 * stroboLength) {
-				stroboCycle = 0;
-			}
-
-			//PWM.loop();
 		}
 	}
 }
-
