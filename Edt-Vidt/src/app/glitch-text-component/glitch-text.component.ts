4b0e757c279988e018cee2c40d91538852e97b6a
Jorinde Reijnierse <jorinde.reijnierse@usmedia.nl>
2017-08-20 15:07:47 +0200

    add keyframes & modifiers

--- Edt-Vidt/src/app/glitch-text-component/glitch-text.component.ts
+++ Edt-Vidt/src/app/glitch-text-component/glitch-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-glitch-text-component',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class GlitchTextComponent implements OnInit {
+
+    @Input() glitchText: string;
 
     constructor() { }
 
