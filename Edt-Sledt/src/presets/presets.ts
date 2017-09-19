7395ecff675ee76dba617187ece31af46a41ef42
Edwin Meijne <git@edwinmeijne.nl>
2017-09-19 21:30:33 +0200

    * included outputs as well now

--- Edt-Sledt/src/presets/presets.ts
+++ Edt-Sledt/src/presets/presets.ts
@@ -1,5 +1,7 @@
 import {GlitchLogo} from './ambient/glitchLogo';
 import {BgColorCycle} from './converters/bgColorCycle';
+import {EdtLEDFollowColor} from './colors/edtLEDFollowColor';
+import {EdtVidtFollowColor} from './colors/edtVidtFollowColor';
 
 /**
  * An Edt-Preset is a `state` that can be active during a performance.
@@ -23,5 +25,9 @@ export const edtPresets = new Map<number, edtPreset>();
 // Ambient effects
 edtPresets.set(0, new GlitchLogo());
 
-// Converters that take input and
+// Converters that take input and convert to 'subjects' like color etc
 edtPresets.set(10, new BgColorCycle());
+
+// Output controls that take 'subjects' or inputs and send this to an output device
+edtPresets.set(30, new EdtLEDFollowColor());
+edtPresets.set(31, new EdtVidtFollowColor());
