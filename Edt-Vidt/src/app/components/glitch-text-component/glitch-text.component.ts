ec8b85b5c3093ea50b805d3696da88f362d3f82e
Jorinde Reijnierse <jorinde.reijnierse@usmedia.nl>
2017-10-11 22:58:08 +0200

    add glitch on track

--- Edt-Vidt/src/app/components/glitch-text-component/glitch-text.component.ts
+++ Edt-Vidt/src/app/components/glitch-text-component/glitch-text.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit, OnDestroy} from '@angular/core';
+import {Component, Input, OnInit, OnDestroy, ElementRef} from '@angular/core';
+import {CommunicationService} from "../../communication.service";
+import {ITrackMsg} from "../../../../../SharedTypes/socket";
 
 @Component({
     selector: 'app-glitch-text-component',
@@ -8,11 +10,53 @@ import {Component, Input, OnInit, OnDestroy} from '@angular/core';
 export class GlitchTextComponent implements OnInit, OnDestroy {
     @Input() glitchText: string;
     @Input() glitchModifier: glitchModifiers;
+    private amount = 8;
 
-    constructor(
-    ) {}
+    constructor(private element: ElementRef, private communicationService: CommunicationService) {
+
+    }
 
     ngOnInit() {
+        this.animateColor();
+        this.communicationService.track.subscribe((track) => {
+            this.amount = Math.max(8, track.right.z);
+            this.animateColor();
+        });
+    }
+
+    animateColor() {
+        const colorElems = this.element.nativeElement.getElementsByClassName('glitch__color');
+        for (const colorElem of colorElems) {
+            colorElem.animate(
+                [
+                    {
+                        transform: `translate(0)`,
+                    },
+                    {
+                        transform: `translate(-${this.amount}px, ${this.amount}px)`
+                    },
+                    {
+                        transform: `translate(-${this.amount}px, -${this.amount}px)`
+                    },
+                    {
+                        transform: `translate(${this.amount}px, ${this.amount}px)`
+                    },
+                    {
+                        transform: `translate(${this.amount}px, -${this.amount}px)`
+                    },
+                    {
+                        transform: `translate(0)`
+                    }
+                ], {
+                    direction: (colorElem.classList.contains('glitch__color--second')) ? "reverse" : "normal",
+                    duration: 300,
+                    delay: (colorElem.classList.contains('glitch__color--third')) ? 100 : 0,
+                    iterations: Infinity,
+                    easing: "cubic-bezier(.25, .46, .45, .94)"
+                }
+            )
+        }
+
     }
 
     ngOnDestroy() {
