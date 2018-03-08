1e7f9c142cac813d40918f01531f0a30ea8967a6
Edwin Meijne <git@edwinmeijne.nl>
2018-03-08 23:23:26 +0100

    * PhotoBounce

--- Edt-Sledt/src/presets/outputs/beatToVidtBounce.ts
+++ Edt-Sledt/src/presets/outputs/beatToVidtBounce.ts
@@ -2,13 +2,17 @@ import {Subscription} from 'rxjs/Subscription';
 import {drumTriggerOn$} from '../../inputs/musicTriggers';
 import {filter} from 'rxjs/operators';
 import {sendToVidt} from '../../outputs/edt-vidt';
-import {IIntensityMsg} from '../../../../SharedTypes/socket';
+import {IIntensityMsg, IPreparePresetMsg, VidtPresets} from '../../../../SharedTypes/socket';
 import {IEdtPresetLogic} from '../presets';
 
 export class BeatToVidtBounce implements IEdtPresetLogic {
     private subscriber: Subscription;
 
     public startPreset(listenTo: number): void {
+        sendToVidt({
+            preset: VidtPresets.PhotoBounce,
+        } as IPreparePresetMsg);
+
         this.subscriber = drumTriggerOn$.pipe(
             filter((drumNote) => listenTo === drumNote)
         )
