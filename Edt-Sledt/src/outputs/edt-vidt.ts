6c8952ef7a6494f3abe096d0703a6f9c28a97bcd
Edwin Meijne <git@edwinmeijne.nl>
2018-04-29 16:37:07 +0200

    * beat and color both need to go to Vidt

--- Edt-Sledt/src/outputs/edt-vidt.ts
+++ Edt-Sledt/src/outputs/edt-vidt.ts
@@ -2,8 +2,18 @@
 
 import {Actions} from '../../../Shared/actions';
 import {io} from '../communication/sockets';
+import {BeatMain} from '../subjects/triggers';
+import {mainColor} from '../subjects/colors';
 
 export function toVidt(msg: Actions): void {
     console.log('Edt-Vidt: ', msg);
     io.emit('toVidt', msg);
 }
+
+BeatMain.subscribe(velocity => {
+    toVidt(Actions.mainBeat(velocity));
+});
+
+mainColor.subscribe(color => {
+    toVidt(Actions.singleColor(color));
+});
