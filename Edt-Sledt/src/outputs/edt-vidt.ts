3dcca08106ed631069a0d4d4bd10a719bcd597ab
Jorinde Reijnierse <jorinde.reijnierse@usmedia.nl>
2018-05-03 22:08:09 +0200

    fixes

--- Edt-Sledt/src/outputs/edt-vidt.ts
+++ Edt-Sledt/src/outputs/edt-vidt.ts
@@ -4,7 +4,7 @@ import {Actions} from '../../../Shared/actions';
 import {io} from '../communication/sockets';
 import {BeatMain$} from '../subjects/triggers';
 import {mainColor$} from '../subjects/colors';
-import {animationType, imageSrc, videoSrc} from '../subjects/vidtSettings';
+import { animationType, imageSrc, imageSrc$, videoSrc, videoSrc$, animationType$ } from '../subjects/vidtSettings';
 
 export function toVidt(msg: Actions): void {
     console.log('Edt-Vidt: ', msg);
@@ -19,14 +19,14 @@ mainColor$.subscribe(color => {
     toVidt(Actions.singleColor(color));
 });
 
-animationType.subscribe(type => {
+animationType$.subscribe(type => {
     toVidt(Actions.animationType(type));
 });
 
-imageSrc.subscribe(src => {
+imageSrc$.subscribe(src => {
     toVidt(Actions.imageSrc(src));
 });
 
-videoSrc.subscribe(src => {
+videoSrc$.subscribe(src => {
     toVidt(Actions.videoSrc(src));
 });
--- Edt-Sledt/src/subjects/vidtSettings.ts
+++ Edt-Sledt/src/subjects/vidtSettings.ts
@@ -1,8 +1,13 @@
 import {animationTypes} from '../../../Shared/vidt-presets';
 import {Subject} from 'rxjs/Subject';
 import {IPhotoAsset, IVideoAsset} from '../../../Shared/assets';
+import { Actions$ } from '../../../Shared/actions';
+import { merge } from 'rxjs/operators';
 
 export const animationType: Subject<animationTypes> = new Subject<animationTypes>();
-
 export const videoSrc: Subject<IVideoAsset> = new Subject<IVideoAsset>();
 export const imageSrc: Subject<IPhotoAsset> = new Subject<IPhotoAsset>();
+
+export const animationType$ = animationType.asObservable().pipe(merge(Actions$.animationType));
+export const imageSrc$ = imageSrc.asObservable().pipe(merge(Actions$.imageSrc));
+export const videoSrc$ = videoSrc.asObservable().pipe(merge(Actions$.videoSrc));
