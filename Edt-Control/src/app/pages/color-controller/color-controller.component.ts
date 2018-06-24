4175aaea264fe4f254b6544f95df9d8e47afca3e
Jorinde Reijnierse <jorinde.reijnierse@usmedia.nl>
2018-06-24 22:14:59 +0200

    styling

--- Edt-Control/src/app/pages/color-controller/color-controller.component.ts
+++ Edt-Control/src/app/pages/color-controller/color-controller.component.ts
@@ -1,13 +1,27 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import { Actions } from '../../../../../Shared/actions';
+import { SocketService } from '../../socket.service';
 
 @Component({
     selector: 'app-color-controller',
-    template: ``,
-    styles: [],
+    templateUrl: './color-controller.component.html',
+    styleUrls: ['./color-controller.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ColorControllerComponent implements OnInit {
 
+    constructor(private socket: SocketService) {
+    }
+
     ngOnInit(): void {
     }
+
+    sendColor(hue, saturation, brightness) {
+        this.socket.toSledt(Actions.singleColor({
+            hue,
+            saturation,
+            brightness,
+        }))
+    }
+
 }
--- Edt-Control/src/app/pages/cues-controller/cues-controller.component.ts
+++ Edt-Control/src/app/pages/cues-controller/cues-controller.component.ts
@@ -3,14 +3,8 @@ import {Actions$} from '../../../../../Shared/actions';
 
 @Component({
     selector: 'app-cues-controller',
-    template: `
-        <ul *ngIf="(cueList$ | async) as cues">
-            <li *ngFor="let cue of cues">
-                <button class="button is-large">{{cue.label}}</button>
-            </li>
-        </ul>
-    `,
-    styles: [],
+    templateUrl: './cues-controller.component.html',
+    styleUrls: ['./cues-controller.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CuesControllerComponent implements OnInit {
