e25f2fe56e3e0bccb511c1fdba992b27cbc55e1c
edwinmeijne <git@edwinmeijne.nl>
2019-06-09 09:40:15 +0200

    * group presets so it's easier to see them in the interface

--- TypeScript/Edt-Control/src/app/pages/preset-controller/preset-controller.component.ts
+++ TypeScript/Edt-Control/src/app/pages/preset-controller/preset-controller.component.ts
@@ -1,24 +1,40 @@
 import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
 import { Actions$ } from "../../../../../Shared/actions";
 import { IControlPresetMsg } from "../../../../../Shared/types";
-
-interface presetStateGrouped {
-  [key: number]: IControlPresetMsg[];
-}
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-preset-controller',
+  selector: "app-preset-controller",
   template: `
-    <ul class="list list--presets" *ngIf="(presetState$ | async) as presets">
-      <li class="list__item" *ngFor="let preset of presets">
-        <app-preset-switcher [preset]="preset"></app-preset-switcher>
-      </li>
-    </ul>
+    <ng-container *ngIf="(presetState$ | async) as presetState">
+      <ng-container *ngFor="let group of presetState">
+        <h1>{{group.title}}</h1>
+        <ul class="list list--presets">
+          <li class="list__item" *ngFor="let preset of group.presets">
+            <app-preset-switcher [preset]="preset"></app-preset-switcher>
+          </li>
+        </ul>
+      </ng-container>
+    </ng-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PresetControllerComponent implements OnInit {
-  presetState$ = Actions$.presetState.asObservable();
+  presetState$ = Actions$.presetState.asObservable().pipe(
+    map((presets: IControlPresetMsg[]) => {
+      return presets.reduce((grouped, preset) => {
+        if (!grouped[preset.config.group]) {
+          grouped[preset.config.group] = {
+            title: preset.config.group,
+            presets: [],
+          };
+        }
+        grouped[preset.config.group].presets.push(preset);
+        return grouped;
+      }, {});
+    }),
+    map(groupedPresets => Object.values(groupedPresets)),
+  );
 
   constructor() {
   }
