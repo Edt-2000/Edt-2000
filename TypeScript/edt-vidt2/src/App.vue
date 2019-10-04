c1e381aacc5a7f6304da0f1a8d9724843faa6f51
edwinmeijne <git@edwinmeijne.nl>
2019-10-04 16:31:32 +0200

    * add socket to Vidt to be controllable from Sledt again

--- TypeScript/edt-vidt2/src/App.vue
+++ TypeScript/edt-vidt2/src/App.vue
@@ -2,6 +2,34 @@
     <router-view/>
 </template>
 
+<script lang="ts">
+    import { Actions$ } from "../../Shared/actions";
+    import router from "./router";
+    import { VidtPresets } from "../../Shared/vidt-presets";
+
+    import { Component } from "vue-property-decorator";
+    import Vue from "vue";
+
+    @Component
+    export default class App extends Vue {
+        public subscription: any;
+
+        mounted() {
+            this.subscription = Actions$.prepareVidt.subscribe(
+                (preset: number) => {
+                    router.push({path: "/" + VidtPresets[preset]});
+                },
+            );
+        }
+
+        destroyed() {
+            if (typeof this.subscription !== "undefined") {
+                this.subscription.unsubscribe();
+            }
+        }
+    }
+</script>
+
 <style lang="scss">
     @import "./scss/layout";
 </style>
