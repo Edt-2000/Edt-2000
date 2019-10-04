abf7cadee196dc5fae71b9617e41a748e87aeff3
edwinmeijne <git@edwinmeijne.nl>
2019-10-04 16:32:21 +0200

    * move all to edt-vidt folder again (makes for a nice git diff)

---
+++ TypeScript/Edt-Vidt/src/main.ts
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueSocketIO from 'vue-socket.io';
+import { vidtSocketConfig } from '../../Shared/config';
+import { nextActionFromMsg } from '../../Shared/actions';
+
+Vue.config.productionTip = false;
+
+Vue.use(new VueSocketIO({
+    connection: vidtSocketConfig.url,
+    options: vidtSocketConfig.options,
+    sockets: {
+        toVidt: nextActionFromMsg,
+    },
+}));
+
+new Vue({
+    router,
+    render: (h) => h(App),
+}).$mount('#app');
