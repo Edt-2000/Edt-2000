7f080bbc2ee84c9e92dd2a13ffea93294be719ee
edwinmeijne <git@edwinmeijne.nl>
2020-01-13 11:23:08 +0100

    * simplify edt-control and edt-vidt pass-through

--- TypeScript/Edt-Sledt/src/outputs/edt-control.ts
+++ TypeScript/Edt-Sledt/src/outputs/edt-control.ts
@@ -1,9 +1,9 @@
 import { fromEvent } from 'rxjs/observable/fromEvent';
-import { Actions, Actions$, nextActionFromMsg } from '../../../Shared/actions';
+import { Actions$, nextActionFromMsg } from '../../../Shared/actions';
 import { take, takeUntil } from 'rxjs/operators';
 import { controlSocket$ } from '../communication/sockets';
 import * as SocketIO from 'socket.io';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, merge } from 'rxjs';
 
 const connectedControlsSubject$ = new BehaviorSubject<string[]>([]);
 
@@ -16,21 +16,15 @@ controlSocket$.subscribe(socket => {
         connectedControlsSubject$.next(Object.keys(socket.nsp.sockets));
     });
 
-    Actions$.presetState.pipe(takeUntil(disconnected$)).subscribe(state => {
-        socket.emit('toControl', Actions.presetState(state));
-    });
-    Actions$.cueList.pipe(takeUntil(disconnected$)).subscribe(list => {
-        socket.emit('toControl', Actions.cueList(list));
-    });
-    Actions$.colorPalette.pipe(takeUntil(disconnected$)).subscribe(colors => {
-        socket.emit('toControl', Actions.colorPalette(colors));
-    });
-    Actions$.contentGroups.pipe(takeUntil(disconnected$)).subscribe(contentGroups => {
-        socket.emit('toControl', Actions.contentGroups(contentGroups));
-    });
-    Actions$.contentGroup.pipe(takeUntil(disconnected$)).subscribe(contentGroup => {
-        socket.emit('toControl', Actions.contentGroup(contentGroup));
-    });
+    merge(
+        Actions$.presetState,
+        Actions$.cueList,
+        Actions$.colorPalette,
+        Actions$.contentGroups,
+        Actions$.contentGroup,
+    ).pipe(
+        takeUntil(disconnected$),
+    ).subscribe(msg => socket.emit('toControl', msg));
 
     socket.on('fromControl', nextActionFromMsg);
 });
--- TypeScript/Edt-Sledt/src/outputs/edt-vidt.ts
+++ TypeScript/Edt-Sledt/src/outputs/edt-vidt.ts
@@ -2,8 +2,8 @@ import { vidtSocket$ } from '../communication/sockets';
 import { fromEvent } from 'rxjs/observable/fromEvent';
 import * as SocketIO from 'socket.io';
 import { take, takeUntil } from 'rxjs/operators';
-import { Actions, Actions$ } from '../../../Shared/actions';
-import { BehaviorSubject } from 'rxjs';
+import { Actions$ } from '../../../Shared/actions';
+import { BehaviorSubject, merge } from 'rxjs';
 
 export const connectedVidtSubject$ = new BehaviorSubject<string[]>([]);
 
@@ -16,38 +16,20 @@ vidtSocket$.subscribe(socket => {
         connectedVidtSubject$.next(Object.keys(socket.nsp.sockets));
     });
 
-    Actions$.animationType.pipe(takeUntil(disconnected$)).subscribe(type => {
-        socket.emit('toVidt', Actions.animationType(type));
-    });
-    Actions$.imageSrc.pipe(takeUntil(disconnected$)).subscribe(src => {
-        socket.emit('toVidt', Actions.imageSrc(src));
-    });
-    Actions$.videoSrc.pipe(takeUntil(disconnected$)).subscribe(src => {
-        socket.emit('toVidt', Actions.videoSrc(src));
-    });
-    Actions$.prepareVidt.pipe(takeUntil(disconnected$)).subscribe(preset => {
-        socket.emit('toVidt', Actions.prepareVidt(preset));
-    });
-    Actions$.mainText.pipe(takeUntil(disconnected$)).subscribe(text => {
-        socket.emit('toVidt', Actions.mainText(text));
-    });
-    Actions$.mainDrum.pipe(takeUntil(disconnected$)).subscribe(drum => {
-        socket.emit('toVidt', Actions.mainDrum(drum));
-    });
-    Actions$.mainBeat.pipe(takeUntil(disconnected$)).subscribe(beat => {
-        socket.emit('toVidt', Actions.mainBeat(beat));
-    });
-    Actions$.vidtSingleColor.pipe(takeUntil(disconnected$)).subscribe(color => {
-        socket.emit('toVidt', Actions.vidtSingleColor(color));
-    });
-    Actions$.vidtMultiColor.pipe(takeUntil(disconnected$)).subscribe(multi => {
-        socket.emit('toVidt', Actions.vidtMultiColor(multi));
-    });
-    Actions$.glitchIntensity
-        .pipe(takeUntil(disconnected$))
-        .subscribe(intensity => {
-            socket.emit('toVidt', Actions.glitchIntensity(intensity));
-        });
+    merge(
+        Actions$.animationType,
+        Actions$.imageSrc,
+        Actions$.videoSrc,
+        Actions$.prepareVidt,
+        Actions$.mainText,
+        Actions$.mainDrum,
+        Actions$.mainBeat,
+        Actions$.vidtSingleColor,
+        Actions$.vidtMultiColor,
+        Actions$.glitchIntensity,
+    ).pipe(
+        takeUntil(disconnected$),
+    ).subscribe(msg => socket.emit('toVidt', msg));
 });
 
 export const connectedVidt$ = connectedVidtSubject$.asObservable();
