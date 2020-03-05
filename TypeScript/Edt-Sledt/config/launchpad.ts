339fac9577c7516401b43335ee8c4970e40fb4ce
edwinmeijne <git@edwinmeijne.nl>
2020-03-05 20:24:42 +0100

    * add first page for testing purposes

--- TypeScript/Edt-Sledt/config/launchpad.ts
+++ TypeScript/Edt-Sledt/config/launchpad.ts
@@ -1,7 +1,10 @@
 import { LaunchpadPage } from '../../Shared/actions/types';
-import { Actions$ } from '../../Shared/actions/actions';
+import { Actions, Actions$ } from '../../Shared/actions/actions';
 import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { VidtPresets } from '../../Shared/vidt-presets';
+import { AnimationTypes } from '../../Shared/vidt/animation';
+import { blackColor } from '../../Shared/colors/utils';
 
 export const launchpadPage$: Observable<LaunchpadPage> = combineLatest([
     Actions$.launchpadPageNr,
@@ -10,9 +13,24 @@ export const launchpadPage$: Observable<LaunchpadPage> = combineLatest([
     Actions$.animationTypes,
     Actions$.contentGroup,
 ]).pipe(
-    map(([pageChange, vidtPresets, colorPalette, animationTypes, contentGroup]) => {
-
-    }),
+    // TODO: Typing of this one is not completely checked for some reason
+    map(([pageNr, vidtPresets, colorPalette, animationTypes, contentGroup]) => ({
+            triggers: [
+                vidtPresets.map(preset => {
+                    return ['yellow', 'amber', preset, Actions.prepareVidt(VidtPresets[preset])];
+                }),
+                animationTypes.map(type => {
+                    return ['red', 'amber', type, Actions.animationType(AnimationTypes[type])];
+                }),
+                colorPalette.map((color, i) => {
+                    return ['green', 'amber', color, Actions.singleColor(colorPalette[i]), Actions.singleColor(blackColor)];
+                }),
+                colorPalette.map((color, i) => {
+                    return ['yellow', 'amber', color, Actions.singleColor(colorPalette[i])];
+                }),
+            ].map(row => [...chunk(row, 9)]).flat(),
+        }),
+    ),
 );
 
 function chunk(arr, n) {
