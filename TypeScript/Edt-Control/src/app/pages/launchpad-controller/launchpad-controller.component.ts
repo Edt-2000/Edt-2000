fd760b242b577e8005e71a9ca321673aacbe5361
JorindeReijnierse <jorinde88@gmail.com>
2021-08-26 21:19:26 +0200

    Styling launchpad

--- TypeScript/Edt-Control/src/app/pages/launchpad-controller/launchpad-controller.component.ts
+++ TypeScript/Edt-Control/src/app/pages/launchpad-controller/launchpad-controller.component.ts
@@ -2,36 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Actions$ } from '../../../../../Shared/actions/actions';
 import { combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
-import {LaunchpadTrigger, TriggerType} from '../../../../../Shared/actions/types';
-import {SocketService} from "../../socket.service";
+import { LaunchpadTrigger, TriggerType } from '../../../../../Shared/actions/types';
+import { SocketService } from "../../socket.service";
 
 @Component({
   selector: 'app-launchpad-controller',
   templateUrl: './launchpad-controller.component.html',
-  styles: [`
-    .launchpad {
-      display: flex;
-      flex-direction: column;
-      height: 100vh;
-    }
-
-    .launchpad__row {
-      display: flex;
-      flex: 0 1 12.5%;
-      width: 100vh; /*Make it square*/
-    }
-
-    .launchpad__item {
-      display: flex;
-      flex: 0 0 12.5%;
-      border: 2px solid red;
-      align-items: center;
-      justify-content: center;
-      background-repeat: no-repeat;
-      background-position: center center;
-      background-size: contain;
-    }
-  `],
+  styleUrls: ['./launchpad-controller.component.scss']
 })
 export class LaunchpadControllerComponent implements OnInit {
   triggerType = TriggerType;
