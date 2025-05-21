96cf253909dd76f10d24b8f8e1d83d5dea3cd793
Edwin Meijne <git@edwinmeijne.nl>
2025-05-21 21:59:52 +0200

    wip: fix #2!

--- TypeScript/Edt-Vidt/src/app/views/spectrum/spectrum.component.ts
+++ TypeScript/Edt-Vidt/src/app/views/spectrum/spectrum.component.ts
@@ -12,54 +12,43 @@ import { AnimationTypes } from '../../../../../Shared/vidt/animation';
     encapsulation: ViewEncapsulation.None,
 })
 export class SpectrumComponent implements OnInit, OnDestroy {
-    public modifier$: Observable<string> =
-        Actions$.animationType.pipe(
-            map((type) => {
-                if (type === AnimationTypes.lava) {
-                    return 'spectrum--lava';
-                } else {
-                    return '';
-                }
-            }),
-        );
+    public modifier$: Observable<string> = Actions$.animationType.pipe(
+        map((type) => {
+            if (type === AnimationTypes.lava) {
+                return 'spectrum--lava';
+            } else {
+                return '';
+            }
+        }),
+    );
 
-    public baseTime$: Observable<number> =
-        Actions$.glitchIntensity.pipe(
-            map((intensity) => {
-                return Math.max((9.5 - intensity), 1.5);
-            }),
-            startWith(4),
-        )
+    public baseTime$: Observable<number> = Actions$.glitchIntensity.pipe(
+        map((intensity) => {
+            return Math.max(9.5 - intensity, 1.5);
+        }),
+        startWith(4),
+    );
 
-    public subbars$: Observable<{rgb: number[]}[]> =
-        Actions$.vidtMultiColor.pipe(
-            withLatestFrom(this.modifier$),
-            map(([colors, modifier]) => {
-                if (!colors || colors.length === 0 || modifier === 'spectrum--lava') {
-                    return [
-                        { rgb: [165, 42 ,42] },
-                        { rgb: [255,140,0] },
-                        { rgb: [255,215,0] },
-                    ]
-                }
+    public subbars$: Observable<{ rgb: number[] }[]> = Actions$.vidtMultiColor.pipe(
+        withLatestFrom(this.modifier$),
+        map(([colors, modifier]) => {
+            if (!colors || colors.length === 0 || modifier === 'spectrum--lava') {
+                return [{ rgb: [165, 42, 42] }, { rgb: [255, 140, 0] }, { rgb: [255, 215, 0] }];
+            }
 
-                // Set colors
-                const color1 = colors[0];
-                const color2 = colors[1] ?? ColorHelper.getContraColor(colors[0]);
-                const color3 = colors[2] ?? colors[0]
+            // Set colors
+            const color1 = colors[0];
+            const color2 = colors[1] ?? ColorHelper.getContraColor(colors[0]);
+            const color3 = colors[2] ?? colors[0];
 
-                return [
-                    { rgb: ColorHelper.hsv2rgb(color1) },
-                    { rgb: ColorHelper.hsv2rgb(color2) },
-                    { rgb: ColorHelper.hsv2rgb(color3) },
-                ]
-            }),
-            startWith([
-                { rgb: [165, 42 ,42] },
-                { rgb: [255,140,0] },
-                { rgb: [255,215,0] },
-            ]),
-        )
+            return [
+                { rgb: ColorHelper.hsv2rgb(color1) },
+                { rgb: ColorHelper.hsv2rgb(color2) },
+                { rgb: ColorHelper.hsv2rgb(color3) },
+            ];
+        }),
+        startWith([{ rgb: [165, 42, 42] }, { rgb: [255, 140, 0] }, { rgb: [255, 215, 0] }]),
+    );
 
     public styles: Record<string, string> = {};
     public columns = createFilledArray(20);
@@ -85,22 +74,30 @@ export class SpectrumComponent implements OnInit, OnDestroy {
     public getDelayModifier(index: number, subbarsLength: number): number {
         const half = Math.floor((subbarsLength - 1) / 2);
         const step = 0.1;
-        const start = 1 - (step * half);
-        return start + (index * step);
+        const start = 1 - step * half;
+        return start + index * step;
     }
 
     public getBarHeight(index: number): number {
         let step = 30;
 
         switch (index) {
-            case 1: step = 30; break;
-            case 2: step = 25; break;
-            case 3: case 4: step = 10; break;
-            default: step = 25;
+            case 1:
+                step = 30;
+                break;
+            case 2:
+                step = 25;
+                break;
+            case 3:
+            case 4:
+                step = 10;
+                break;
+            default:
+                step = 25;
         }
 
         const start = 100;
-        return start - (index * step);
+        return start - index * step;
     }
 
     public ngOnDestroy() {
